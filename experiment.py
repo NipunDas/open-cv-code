6b48fc82133f4f86952f08179200bbcf7e38e062
NipunDas <nipundas14@gmail.com>
2021-06-15 01:24:04 -0700

    working on object detection

--- experiment.py
+++ experiment.py
@@ -1,19 +1,30 @@
 import cv2 as cv
+import numpy as np
 
 capture = cv.VideoCapture(0)
 
 def analyzeFrame(frame):
-	#blur = cv.GaussianBlur(frame, (5,5), cv.BORDER_DEFAULT)
-	#canny = cv.Canny(blur, 125, 175)
-	#dilated = cv.dilate(canny, (7,7), iterations=3)
 	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-	ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
-	return thresh
+	blur = cv.GaussianBlur(gray, (9,9), cv.BORDER_DEFAULT)
+	canny = cv.Canny(blur, 100, 200)
+	dilated = cv.dilate(canny, (7,7), iterations=3)
+	#eroded = cv.erode(canny, (3,3), iterations=1)
+	contours, hierarchies = cv.findContours(dilated, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+	blank = np.zeros(frame.shape, dtype='uint8')
+
+	for contour in contours:
+		(x,y,w,h) = cv.boundingRect(contour)
+		if w > 100 & w < 1000:
+			cv.rectangle(blur, (x,y), (x+w,y+h), (0,255,0), thickness=1)
+
+	#cv.drawContours(blank, contours, -1, (0,0,255), 2)
+	cv.imshow('Contours', blur)
+	cv.imshow('Edges', dilated)
 
 while True:
 	isTrue, frame = capture.read()
-	analyzedFrame = analyzeFrame(frame)
-	cv.imshow('Webcam', analyzedFrame)
+	analyzeFrame(frame)
+	#cv.imshow('Webcam', frame)
 
 	if cv.waitKey(20) & 0xFF==ord('d'):
 		break
